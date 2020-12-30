a6b1ffa31d5c48ff9a18c63d3ab2badeda319f4f
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-30 08:34:04 +0000

    operand

--- nemu/include/cpu/decode/operand.h
+++ nemu/include/cpu/decode/operand.h
@@ -1,7 +1,7 @@
 #ifndef __OPERAND_H__
 #define __OPERAND_H__
 
-enum { OP_TYPE_REG, OP_TYPE_MEM, OP_TYPE_IMM };
+enum { OP_TYPE_REG, OP_TYPE_MEM, OP_TYPE_IMM, OP_TYPE_NO };
 
 #define OP_STR_SIZE 40
 
@@ -10,7 +10,10 @@ typedef struct {
 	size_t size;
 	union {
 		uint32_t reg;
+          struct{
 		swaddr_t addr;
+                uint8_t sreg;
+          };
 		uint32_t imm;
 		int32_t simm;
 	};
@@ -20,7 +23,8 @@ typedef struct {
 
 typedef struct {
 	uint32_t opcode;
-	bool is_operand_size_16;
+	bool is_data_size_16;
+        bool is_stack_size_16;
 	Operand src, dest, src2;
 } Operands;
 
